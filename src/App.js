f8b0137feafe3d5916d38244fcd2c05c55ed485b
Ankit chopra <chopraankit1608@gmail.com>
2023-08-16 14:27:24 +0530

    this is nakit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Mates....!!!</h1>
-      <h2>Hello Brothers</h2>
-      <h3>
-        hello buddies!!
-      </h3>
+     
     </div>
   );
 }
